06cd7850308aa42f0919887fd608d68ebfef6a39
Debbie Rios <debbie.rios@jalasoft.com>
2013-07-18 11:02:24 -0400

    Update doc
    Add examples in the doc parameters.

--- Code/Algorithm.py
+++ Code/Algorithm.py
@@ -13,8 +13,10 @@ class Algorithm():
 
     def __init__(self, grids):
         """
-        Constructor fo the class Algorthm
-        grids: is the sudoku puzzle  without resolve.
+        Constructor fo the class Algorthm.
+
+        Keyword arguments:
+        grid -- the grid of the sudoku  to resolve. e.g "00302060...."
         """
         self.grids = grids
 
@@ -23,7 +25,9 @@ class Algorithm():
         """
         Static method used to read a file, return the file parsed into a
         string.
-        filename: name of the xml file.
+
+        Keyword arguments:
+        filename: name of file e.g 'sudoku.txt'.
         """
         parsedString = file(filename).read()
         return parsedString
@@ -31,8 +35,8 @@ class Algorithm():
     def convertStringToMatrix(self, grids):
         """
         This method receive the grid into a string and return the grid into
-        a matrix.
-        grids: is the sudoku puzzle  without resolve.
+        a matrix e.g [ [0,0,3,0,2,0,6,0,8], [1,0,0,4,5,6,0,8,,], ...., []]
+        grid -- the grid of the sudoku  to resolve. e.g "00302060...."
         """
         lisOfValues = []
         matrix = [[0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0],\
