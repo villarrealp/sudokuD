35036a40c386c1f92c4e3c65ff7b908bbd391060
Debbie Rios <debbie.rios@jalasoft.com>
2013-07-18 12:52:46 -0400

    Update some observations.

--- Code/Algorithm.py
+++ Code/Algorithm.py
@@ -1,54 +1,61 @@
 """
 Author: Debbie Rios
 Date: 07/04/2013
-Modified:  07/17/2013    by: Debbie Rios
+Modified:  07/18/2013    by: Debbie Rios
 Comments: Changes in the methods, follows standards.
 """
 """
 Class Name: Algorithm
-Description: receive a grid to solve the sudoku
+Description: receive string with the grid to solve the sudoku
 """
 
 class Algorithm():
 
     def __init__(self, grids):
         """
-        Constructor fo the class Algorthm.
+        Constructor fo the class Algorithm.
 
         Keyword arguments:
-        grid -- the grid of the sudoku  to resolve. e.g "00302060...."
+        grids -- string with grid of the sudoku to resolve, 81 non-blank
+        chars e.g "00302060...."
         """
         self.grids = grids
 
     @staticmethod
-    def fromFile(filename):
+    def fileToString(filename):
         """
-        Static method used to read a file, return the file parsed into a
-        string.
+        Static method used to read a file.
 
         Keyword arguments:
         filename: name of file e.g 'sudoku.txt'.
+
+        Return:
+        the file parsed into a string e.g. {012008900\n236100789\n.....}
         """
         parsedString = file(filename).read()
         return parsedString
 
     def convertStringToMatrix(self, grids):
         """
-        This method receive the grid into a string and return the grid into
-        a matrix e.g [ [0,0,3,0,2,0,6,0,8], [1,0,0,4,5,6,0,8,,], ...., []]
+        This method receive the grid into a string.
+
+        Keyword arguments:
         grid -- the grid of the sudoku  to resolve. e.g "00302060...."
+
+        Return:
+        the grid into a matrix e.g [ [0,0,3,0,2,0,6,0,8], [1,0,0,4,5,6,0,8,,], ...., []]
         """
         lisOfValues = []
-        matrix = [[0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0],\
-                 [0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0], \
+        matrix = [[0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0],
                  [0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0]]
 
         for grid in grids:
             lisOfValues.append(int(grid))
         count = 0
-        for x in range(9):
-            for y in range(9):
-                matrix[x][y] = lisOfValues[count]
+        for row in range(9):
+            for col in range(9):
+                matrix[row][col] = lisOfValues[count]
                 count  = count + 1
         return matrix
 
--- Code/PeterNorvigAlgorithm.py
+++ Code/PeterNorvigAlgorithm.py
@@ -14,22 +14,22 @@ Class Name: PeterNorvigAlgorithm
 Description: resolve sudoku puzzle, using constraint propagation
 and search methods.
 Throughout this program we have:
-   square, e.g. 'A3'
-   digit,  e.g. '9'
    u is a unit,   e.g. ['A1','B1','C1','D1','E1','F1','G1','H1','I1']
    grid is a grid,e.g. 81 non-blank chars, e.g. starting with '.18...7...
    values is a dict of possible values, e.g. {'A1':'12349', 'A2':'8', ...}
 """
 
 class PeterNorvigAlgorithm(Algorithm):
-    def cross(self, Rows, Cols):
+    def crossProduct(self, Rows, Cols):
         """
-        This method return the cross product of elements in
-        A and elements in B.
+        This method receive two strings.
 
         Keyword arguments:
-        Rows -- elements of a row e.g. 'A'
-        Cols -- elements of a cols e.g. '2'
+        Rows -- string of elements of a row e.g. 'ABCDEFGHI'
+        Cols -- string of elements of a cols e.g. '123456789'
+
+        Return:
+        The cross product of elements in Rows and elements in Cols.
         """
         return [row + col for row in Rows for col in Cols]
 
@@ -40,17 +40,17 @@ class PeterNorvigAlgorithm(Algorithm):
         grids -- the string data of the sudoku without resoved.e.g "00302060...."
         sep -- each file of 9 should be sep for '\n'
         """
-        self.digits   = '123456789' # Valid digits of sudoku grid will contain.
-        self.rows     = 'ABCDEFGHI' # Each row of the sudoku grid will contain.
-        self.cols     = self.digits # Each col of sudoku grid will contain.
+        self.digits = '123456789' # Valid digits of sudoku grid will contain.
+        self.rows = 'ABCDEFGHI' # Each row of the sudoku grid will contain.
+        self.cols = self.digits # Each col of sudoku grid will contain.
 
         # Each square of sudoku grid, 9 squares per sudoku e.g 'A3'
-        self.squares  = self.cross(self.rows, self.cols)
+        self.squares = self.crossProduct(self.rows, self.cols)
 
         # List of units in  the sudoku
-        self.unitlist = ([self.cross(self.rows, col) for col in self.cols] +
-                    [self.cross(row, self.cols) for row in self.rows] +
-                    [self.cross(rs, cs) for rs in ('ABC', 'DEF', 'GHI')\
+        self.unitlist = ([self.crossProduct(self.rows, col) for col in self.cols] +
+                    [self.crossProduct(row, self.cols) for row in self.rows] +
+                    [self.crossProduct(rs, cs) for rs in ('ABC', 'DEF', 'GHI')\
                      for cs in ('123', '456', '789')])
 
         # Each unit of sudoku into a dictionary e.g ['A1','B1',...,'I1']
@@ -66,43 +66,51 @@ class PeterNorvigAlgorithm(Algorithm):
 
     def parseGrid(self, grid):
         """
-        This method convert a grid to a dict of possible values,
-        and return:{square: digits}, or return False if a contradiction is detected.
+        This method convert a grid to a dict of possible values.
 
         Keyword arguments:
-        grid -- the grid of the sudoku  to resolve. e.g "00302060...."
+        grid -- string whit a grid of the sudoku to resolve. e.g "00302060...."
+
+        Return:
+        A dic of {square: digits}, or return False if a contradiction is detected.
         """
         # To start, every square can be any digit; then assign values from the grid.
         values = dict((square, self.digits) for square in self.squares)
         for square, digit in self.getGridValues(grid).items():
-            if digit in self.digits and not self.assign(values, square, digit):
+            if digit in self.digits and not self.assignPosibleValues(values, square, digit):
                 return False # (Fail if we can't assign digit to square square.)
         return values
 
     def getGridValues(self, grid):
         """
-        This method convert a grid into a dict and return:{square: char}
-        with '0' or '.' for empties.
+        This method convert a grid into a dict.
 
         Keyword arguments:
         grid -- the grid of the sudoku  to resolve. e.g "00302060...."
+
+        Return:
+        A dic of {square: char} with '0' or '.' for empties
+        e.g. {'A1' = '123008945'... etc}
         """
-        chars = [c for c in grid if c in self.digits or c in '0.']
+        chars = [eachChar for eachChar in grid if eachChar in self.digits or eachChar in '0.']
         assert len(chars) == 81
         return dict(zip(self.squares, chars))
 
-    def assign(self, values, square, digit):
+    def assignPosibleValues(self, values, square, digit):
         """
         Constraint Propagation method:
         Eliminate all the other values (except digit) from values[square]
         and propagate.
-        Return values, except return False if a contradiction is detected.
 
         Keyword arguments:
         value -- dict of possible values to resolve the sudoku
         e.g. {'A1':'12349', 'A2':'8', ...}
-        square -- each unit of the matriz of the sudoku e.g. I9
-        digit -- each possible digit to resolve the sudoku e.g '8'
+        square -- string of each unit of the matriz of the sudoku e.g. I9
+        digit -- string of each possible digit to resolve the sudoku e.g '8'
+
+        Return:
+        This method return values, except return False if a contradiction
+        is detected.
         """
         other_values = values[square].replace(digit, '')
         if all(self.eliminate(values, square, nextDigit)\
@@ -115,13 +123,16 @@ class PeterNorvigAlgorithm(Algorithm):
         """
         This method eliminate digit from values[square]; propagate when
         values or places <= 2.
-        Return values, except return False if a contradiction is detected.
 
         Keyword arguments:
         values -- dict of possible values to resolve the sudoku
         e.g. {'A1':'12349', 'A2':'8', ...}
         square -- each element of the matriz of the sudoku e.g 'H1'
         digit -- each possible digit to resolve the sudoku. e.g '5'
+
+        Return:
+        This method return values, except return False if a contradiction is
+        detected.
         """
         if digit not in values[square]:
             return values
@@ -134,12 +145,15 @@ class PeterNorvigAlgorithm(Algorithm):
         """
         This method eliminate nextDigit from the peers if a square s is reduced
         to one value nextDigit.
-        Return True, except return False if a contradiction is detected.
 
         Keyword arguments:
         values -- dict of possible values to resolve the sudoku,
         e.g. {'A1':'12349', 'A2':'8', ...}
-        square -- each element of the matriz of the sudoku e.g 'H1'
+        square -- string of each element of the matriz of the sudoku e.g 'H1'
+
+        Return:
+        This method returns True, except return False if a contradiction
+        is detected.
         """
         if len(values[square]) == 0:
             return False                     # Contradiction: removed last value
@@ -154,13 +168,16 @@ class PeterNorvigAlgorithm(Algorithm):
         """
         This method puts a value digit if a unit 'unit' is reduced to only
         one place for a value digit.
-        Return values, except return False if a contradiction is detected.
 
         Keyword arguments:
         values -- dict of possible values to resolve the sudoku,
         e.g. {'A1':'12349', 'A2':'8', ...}
-        square -- each element of the matriz of the sudoku, e.g 'H1'
-        digit -- each possible digit to resolve the sudoku, e.g. '9'
+        square -- string of each element of the matriz of the sudoku, e.g 'H1'
+        digit -- string of each possible digit to resolve the sudoku, e.g. '9'
+
+        Return:
+        This method return values, except return False if a contradiction
+        is detected.
         """
         for unit in self.units[square]:
             dplaces = [square for square in unit if digit in values[square]]
@@ -172,15 +189,17 @@ class PeterNorvigAlgorithm(Algorithm):
                     return False
         return values
 
-
-    def search(self, values):
+    def deepSearch(self, values):
         """
         This method use depth-first search and propagation, recursively
-        try all possible values, return the sudoku solved to solve method.
+        try all possible values.
 
         Keyword arguments:
         values -- dict of possible values to resolve the sudoku,
         e.g. {'A1':'12349', 'A2':'8', ...}
+
+        Return:
+        the sudoku solved with search method.
         """
         if values is False:
             return False                                        # Failed earlier
@@ -189,7 +208,7 @@ class PeterNorvigAlgorithm(Algorithm):
         # Chose the unfilled square square with the fewest possibilities
         n, square = min((len(values[square]), square) for square in self.squares \
                                         if len(values[square]) > 1)
-        return self.validValues(self.search(self.assign(values.copy(), \
+        return self.validValues(self.deepSearch(self.assign(values.copy(), \
                         square, digit)) for digit in values[square])
 
     def validValues(self, seq):
@@ -198,12 +217,12 @@ class PeterNorvigAlgorithm(Algorithm):
         Keyword arguments:
         seq -- the sequence of values to verify for a true value e.g '1234...'
         """
-        for e in seq:
-            if e:
-                return e
+        for element in seq:
+            if element:
+                return element
         return False
 
-    def shuffled(seq):
+    def shuffled(self, seq):
         """
         This method return a randomly shuffled copy of the input sequence.
 
@@ -215,7 +234,7 @@ class PeterNorvigAlgorithm(Algorithm):
         return seq
 
     def getMatrix(self, values):
-        """ This method return the sudoku resolve into a matrix.
+        """ This method return the sudoku into a matrix.
 
         Keyword arguments:
         values -- dict of possible values to resolve the sudoku,
@@ -235,40 +254,26 @@ class PeterNorvigAlgorithm(Algorithm):
         Keyword arguments:
         grid -- the grid of the sudoku  to resolve. e.g "00302060...."
         """
-        return self.search(self.parseGrid(grid))
+        return self.deepSearch(self.parseGrid(grid))
 
-    def solveSudoku(self ):
+    def solveSudoku(self):
         """
         Attempt to solve a sequence of grids. Report results.
         """
-        def time_solve(grid):
+        def timeSolve(grid):
             """
             This method displays the time that its take to solve any sudoku.
 
             Keyword arguments:
             grid -- the grid of the sudoku  to resolve. e.g "00302060...."
+
+            Return:
+            The time that takes resolve each sudoko.
             """
             start = time.clock()
             values = self.solve(grid)
             self.runningTime = time.clock() - start
             self.getMatrix(values)
             return (self.runningTime, self.solved(values))
-        times, results = zip( * [time_solve(grid) for grid in self.grids])
-
-    def display(self, values):
-        """
-        This method display these values as a 2-D grid, return in console the
-        sudoku resolved.
-
-        Keyword arguments:
-        values -- dict of possible values to resolve the sudoku,
-        e.g. {'A1':'12349', 'A2':'8', ...}
-        """
-
-        width = 1 + max(len(values[square]) for square in self.squares)
-        line = '+'.join(['-' * (width * 3)] * 3)
-        for row in self.rows:
-            print ''.join(values[row + col].center(width) + ('|' if col in '36' else '')
-                          for col in self.cols)
-            if row in 'CF': print line
+        times, results = zip( * [timeSolve(grid) for grid in self.grids])
 
